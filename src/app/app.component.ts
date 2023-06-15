05e47cf875223c733f62796ad2f92344c9ebd6bf
Filipe Freire <filipe.freire32@gmail.com>
2023-06-15 03:13:02 +0100

    Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-openfin-app';
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    try {
+      fin.desktop.main(() => this.initWithOpenFin());
+
+    } catch (err) {
+      this.initNoOpenFin();
+    }
+  };
+
+  initWithOpenFin() {
+    const mainWin = fin.desktop.Window.getCurrent();
+    console.log(mainWin);
+
+    fin.desktop.System.getVersion( (version) => {
+      try {
+        console.log("OpenFin version " + version);
+        
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+  }
+
+  initNoOpenFin() {
+    alert("OpenFin is not available - you are probably running in a browser.");
+  }
 }
